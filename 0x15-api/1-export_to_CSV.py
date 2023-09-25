65cb6910c602c346e60298973e622d9dfdb4a547
Sue609 <susankamau609@gmail.com>
2023-09-25 12:08:12 -0700

    Export to CSV.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
-import json
+"""Exports to-do list information for a given employee ID to CSV format."""
+import csv
 import requests
 import sys
 
 
-def export_to_json(user_id):
+def export_to_csv(user_id):
     """
-    Handles the export to JSON format.
+    Function that exports user id to csv.
     """
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    data = {str(user_id): []}
-
-    for todo in todos:
-        task_data = {
-            "task": todo.get("title"),
-            "completed": todo.get("completed"),
-            "username": username
-        }
-        data[str(user_id)].append(task_data)
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(data, jsonfile, indent=4)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    export_to_json(employee_id)
-
+    user_id = sys.argv[1]
+    export_to_csv(user_id)
