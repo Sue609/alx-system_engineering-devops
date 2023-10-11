6273e01af996e82431309398f3041a7b56788910
Sue609 <susankamau609@gmail.com>
2023-10-11 01:40:13 -0700

    recursive function that queries the Reddit API

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module introduces a function.
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after=None, counts=None):
+    """
+    Recursive function that queries and counts occurences of
+    keywords in the reddit hot article titles.
+    """
+    if counts is None:
+        counts = {}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 100}
+
+    if after:
+        params['after'] = after
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        after = data['data']['after']
+
+        for post in posts:
+            title = post['data']['title'].lower()
+            for keyword in word_list:
+                if keyword.lower() in title:
+                    counts[keyword.lower()] = counts.get(keyword, 0) + 1
+        if after:
+            count_words(subreddit, word_list, after, counts)
+        else:
+            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+            for keyword, count in sorted_counts:
+                print(f"{keyword.lower()}: {count}")
