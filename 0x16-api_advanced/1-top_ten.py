0ff07279de12493b13573460e3cfdd21635f0868
Sue609 <susankamau609@gmail.com>
2023-10-10 23:41:06 -0700

    Api that retrieves the first 10 posts.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pyhton3
+"""
+Module that introduces a function.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries and prints the titles of the first 10 hot posts.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts[:10]:
+            title = post['data']['title']
+            print(title)
+    elif response.status_code == 404:
+        print(None)
+    else:
+        print(None)
