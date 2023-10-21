1a5c336a8e157253a16dfb38496d3f95f3b6a882
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-10-21 21:26:57 +0300

    Change the OS configuration

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# changes the OS configuration
+exec { 'change soft limit':
+    command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+    provider => shell,
+}
+
+exec { 'change hard limit':
+    command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+    provider => shell,
+}
